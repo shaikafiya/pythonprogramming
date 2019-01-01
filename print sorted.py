cc365e5eaf3a3b09eedbb8629a5573154e0a7e24
shaikafiya <42992703+shaikafiya@users.noreply.github.com>
2019-01-01 08:34:35 +0530

    Update print sorted.py

--- print sorted.py
+++ print sorted.py
@@ -1,8 +1,7 @@
-m= int(input())
+k= int(input())
 n=[]
-for i in range(m):
+for i in range(k):
     n.extend(input().split())
 n=[int(x) for x in n]
 n.sort()
-for i in n: print(i,end=" ")
-             
+for i in n: print(i,end="" )
