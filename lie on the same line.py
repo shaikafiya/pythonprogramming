4afac23a913eeea134b90b6b026078827c0c2737
shaikafiya <42992703+shaikafiya@users.noreply.github.com>
2019-01-05 10:43:56 +0530

    Update lie on the same line.py

--- lie on the same line.py
+++ lie on the same line.py
@@ -1,9 +1,9 @@
-x1,y1=map(int,input().split())
-x2,y2=map(int,input().split())
-x3,y3=map(int,input().split())
-if((x1==x1==x3) or (y1==y2==y3)):
+x1,z1=map(int,input().split())
+x2,z2=map(int,input().split())
+x3,z3=map(int,input().split())
+if((x1==x1==x3) or (z1==z2==z3)):
     print("yes")
-elif(((y2-y1)/(x2-x1))==((y3-y1)/(x3-x1))):
-    print("yes")
+elif(((z2-z1)/(x2-x1))==((z3-z1)/(x3-x1))): 
+     print("yes")
 else:
     print("no")
