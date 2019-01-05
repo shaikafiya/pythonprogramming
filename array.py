98ffc893113d40a54eeb60749d273608f2bb672f
shaikafiya <42992703+shaikafiya@users.noreply.github.com>
2019-01-05 10:00:51 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,11 +1,11 @@
-n=int(input())
+m=int(input())
 l=list(map(int,input().split()))
-m=l[0]
+n=l[0]
 k=[]
-k.append(m)
+k.append(n)
 for x in range(1,len(l)):
-    m=m+l[x] 
-    k.append(m)
+    n=n+l[x] 
+    k.append(n)
 k.append(l[len(l)-1])
 a=l[len(l)-1]
 for y in range(len(l)-2,-1,-1):
