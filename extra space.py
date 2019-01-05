825277e585e779c0537371fdcccd7c79823b2e5a
shaikafiya <42992703+shaikafiya@users.noreply.github.com>
2019-01-05 14:45:32 +0530

    Update extra space.py

--- extra space.py
+++ extra space.py
@@ -1,16 +1,16 @@
-m=input()
-a=list(m)
-c=len(a)-1
-for i in range(c):
-    if a[i]==" ":
-       a.remove(a[i])
-       if i!=c-1:
-          i=i+1
-          c=c-1
-       else:
-           break
-n="".join(a)
-print(n)
+b=str(input())
+c=b.replace(" "," ")
+print(c)
+
+    
+
+   
+    
+
+
+ 
+   
+
           
     
 
