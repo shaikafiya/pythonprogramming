a7426b60c92eeba5839a6f72d5a5c5e13dd9f173
shaikafiya <42992703+shaikafiya@users.noreply.github.com>
2019-02-18 12:47:34 +0530

    Create creating subarry.py

---
+++ creating subarry.py
@@ -0,0 +1,30 @@
+n=int(input())
+arr=list(map(int,input().split()))
+n=len(arr)
+t=[]
+for i in range(0,n):
+  sum=arr[i]
+  j=i+1
+  c=1
+  if sum<0:
+    flag=0
+    sign='n'
+  else:
+    flag=1
+    sign='p'
+  while j<n:
+    if flag==0 and sign=='n' and arr[j]>0:
+      j+=1
+      c+=1
+      flag=1
+      sign='p'
+    elif flag==1 and sign=='p' and arr[j]<0:
+      j+=1
+      c+=1
+      flag=0
+      sign='n'
+    else:
+      break
+  t.append(c)
+print(*t)
+
