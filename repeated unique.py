49ac5af18fa7f37428d28da7e1d79ae2e7027e2b
shaikafiya <42992703+shaikafiya@users.noreply.github.com>
2019-02-21 11:40:46 +0530

    Create repeated unique.py

---
+++ repeated unique.py
@@ -0,0 +1,16 @@
+b=int(input())
+l=input().split()
+a=" ".join(map(str,l))
+m=[]
+for x in a:
+    if(x not in m):
+        if(a.count(x)>1):
+            m.append(x)
+if(len(m)>1):
+    n=" ".join(m) 
+    h=n[0:1]
+    print(h)
+else:
+    print("unique")
+
+   
