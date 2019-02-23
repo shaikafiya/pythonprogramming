84376cf86e7b19e9d54566a79feb4f23073f947f
shaikafiya <42992703+shaikafiya@users.noreply.github.com>
2019-02-23 11:53:16 +0530

    Update apperead in all array.py

--- apperead in all array.py
+++ apperead in all array.py
@@ -1,10 +1,10 @@
 m,k=map(int,(input()).split())
 a=[]
-b=[]
+c=[]
 for i in range(m):
     a.append(list(map(int,(input()).split())))
 for i in range(k):
-    if all(a[0][i] in a[j]for j in range(1,n)):
+    if all(a[0][i] in a[j]for j in range(1,m)):
         c=c+[a[0][i]]
 c=sorted(c)
 print(" ".join(str(i) for i in c))
